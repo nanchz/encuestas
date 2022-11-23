2139496a847cdeb619b1c5bf1b7655cdbf85b0ab
Andresyfr <andresyfr@gmail.com>
2022-11-23 09:39:16 -0500

    Initial comit for develop

---
+++ src/test/java/com/andresyfr/encuestas/EncuestasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresyfr.encuestas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncuestasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
